18cb1b3d631dc350b3d7f8cff210b1367093ee67
powerofwind <powerofwind10@gmail.com>
2020-05-21 19:48:48 +0700

    call api and binding in order stage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { OrderSendSuccessPage } from '../order-send-success/order-send-success.page';
 import { NativeService } from '../../providers/navigateService';
@@ -14,12 +14,13 @@ import { stringify } from 'querystring';
 export class HomePage implements OnInit {
 
   //MOCK ORDER ID
-  orderId = "637255589336844832";
-
+  orderId = "637256612560183609";
+  testx: string;
   bikerInfo$ = Promise.resolve([]);
   order$ = Promise.resolve([]);
   IsBikerOn: boolean;
-  constructor(public router: Router, private modalController: ModalController, private nativeSvc: NativeService, private bikerSvc: BikerService) { }
+  constructor(public router: Router, private route: ActivatedRoute, private modalController: ModalController, private nativeSvc: NativeService, private bikerSvc: BikerService) {
+  }
 
   // async zzz() {
   //   const modal = await this.modalController.create({
@@ -34,10 +35,35 @@ export class HomePage implements OnInit {
   ngOnInit() {
     this.bikerInfo$ = this.bikerSvc.getBikerInfo();
     this.getBikerStatusAndOrder();
+
+    this.route.params.subscribe(param => { this.testx = param["test"] });
+    console.log(this.testx);
+    this.openModal(this.testx);
+  }
+
+  async openModal(xx: string) {
+    console.log('xxxxxxx', xx);
+    if ((xx != null) && (xx != undefined)) {
+      console.log('555555555555');
+      const modal = await this.modalController.create({
+        component: OrderSendSuccessPage,
+        cssClass: 'dialog-modal-4-order-success',
+        backdropDismiss: false
+      });
+      modal.onDidDismiss().then(it => {
+        console.log(it);
+        this.IsBikerOn = it?.data
+        console.log("IsBikerOn: " ,this.IsBikerOn);
+        this.toggleChange()
+      })
+      modal.present();
+    }
   }
 
   getBikerStatusAndOrder() {
     this.bikerInfo$.then((it: any) => {
+      console.log(it);
+
       this.IsBikerOn = it?.onWorkStatus;
       console.log("get: " + this.IsBikerOn);
 
@@ -51,7 +77,9 @@ export class HomePage implements OnInit {
   }
 
   receiveOrder() {
-    this.nativeSvc.NavigateToPage("order-stage", { id: "id001" });
+    this.bikerSvc.updateOrderStatusToReceived(this.orderId).then(it => {
+      this.nativeSvc.NavigateToPage("order-stage", { id: "id001" });
+    })
   }
 
   toggleChange() {
