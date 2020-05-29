5323c86d9a6c1334b207502819b3b6654b778fa7
tachapong <tachapong999@gmail.com>
2020-05-29 11:49:21 +0700

    validate

--- src/app/order-cancel/order-cancel.page.ts
+++ src/app/order-cancel/order-cancel.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BikerService } from 'src/services/biker.service';
 import { NativeService } from 'src/providers/NativeService';
 import { ActivatedRoute } from '@angular/router';
@@ -19,7 +19,7 @@ export class OrderCancelPage implements OnInit {
     this.route.params.subscribe(param => { this.orderId = param["orderId"] });
     console.log('con', this.orderId);
     this.fg = this.fb.group({
-      'heading': null,
+      'heading': [null, Validators.required],
       'info': null,
     })
   }
@@ -30,10 +30,7 @@ export class OrderCancelPage implements OnInit {
   }
 
   handleSubmit() {
-    console.log(this.orderId);
-    console.log('heading', this.fg.get('heading').value);
-    console.log('heading2', this.fg.get('info').value);
-    if (this.fg.get('heading').value != null && this.fg.get('info').value != null)
+    if (this.fg.valid)
       this.bikerSvc.createOrderCancelRequest(this.orderId, this.fg.value).then((it: any) => {
         this.nativeSvc.GoBack();
       })
