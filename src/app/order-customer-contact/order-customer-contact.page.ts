455d889cbcc7dc70100ab24474d292ca19164b9f
tachapong <tachapong999@gmail.com>
2020-06-12 15:05:48 +0700

    alert error

--- src/app/order-customer-contact/order-customer-contact.page.ts
+++ src/app/order-customer-contact/order-customer-contact.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NativeService } from 'src/providers/NativeService';
 import { BikerService } from 'src/services/biker.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-order-customer-contact',
@@ -12,17 +13,37 @@ export class OrderCustomerContactPage implements OnInit {
   public order$ = Promise.resolve([]);
   public phoneNo: string;
 
-  constructor(private nativeSvc: NativeService, private bikerSvc: BikerService) { }
+  constructor(private nativeSvc: NativeService, private bikerSvc: BikerService, public alertController: AlertController) {
+
+   }
 
   ngOnInit() {
     this.nativeSvc.SetPageTitle("ติดต่อลูกค้า");
-    this.order$ = this.bikerSvc.getOrderInfo();
-    this.order$.then((it: any) => {
-      this.phoneNo = it.customer.tel;
-    })
+    this.loadData();
   }
 
   calling() {
     this.nativeSvc.PhoneCall(this.phoneNo);
+  }
+
+  async loadData() {
+    const alert = await this.alertController.create({
+      header: 'เกิดข้อผิดพลาด',
+      message: "",
+      buttons: [{
+        text: 'ตกลง',
+        handler: () => {
+          // this.navCtrl.back();
+        },
+      }],
+      backdropDismiss: false
+    });
+    this.order$ = this.bikerSvc.getOrderInfo();
+    this.order$.then((it: any) => {
+      this.phoneNo = it.customer.tel;
+    }, async error => {
+      alert.message = error.error.message;
+      await alert.present();
+    });
   }
 }
